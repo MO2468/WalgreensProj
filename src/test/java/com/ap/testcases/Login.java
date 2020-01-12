14942f4f456feeeb79763560d3aa55e6d8ba54e8
MO2468 <islam.m1137@gmail.com>
2020-01-12 15:14:39 -0500

    initial push 1/12/20

---
+++ src/test/java/com/ap/testcases/Login.java
@@ -0,0 +1,44 @@
+package com.ap.testcases;
+
+import java.io.IOException;
+
+
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.ap.base.TestBase;
+import com.ap.pages.HomePage;
+import com.ap.pages.SignInPage;
+
+public class Login extends TestBase{
+	@BeforeTest
+	public void initializer() throws IOException{
+		driver=initialization();
+
+	}
+	
+	@Test
+	public void login (){
+		driver.get(propt.getProperty("url"));
+		HomePage hp = new HomePage(driver);
+	//	Actions a = new Actions(driver);
+	//	a.moveToElement(hp.clickYourAccount()).build().perform();
+		hp.clickYourAccount().click();
+		hp.clickSignIn().click();
+		SignInPage sip = new SignInPage(driver);
+		String SItitle=driver.getTitle();
+		Assert.assertEquals(SItitle, "Sign In or Register to Get Started Using Walgreens.com");
+
+	
+	}
+
+	@AfterTest
+	public void tearDown(){
+		driver.quit();
+		driver=null;
+	}
+
+}
