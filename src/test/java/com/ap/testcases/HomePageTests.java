a0d87db5e7202c646942d17d866ab1a90c267815
MO2468 <islam.m1137@gmail.com>
2020-01-19 20:57:54 -0500

    reverting link class changes plus started story PSQA-6

--- src/test/java/com/ap/testcases/HomePageTests.java
+++ src/test/java/com/ap/testcases/HomePageTests.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 
 
 import org.openqa.selenium.interactions.Actions;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -14,21 +15,34 @@ import com.ap.base.TestBase;
 import com.ap.pages.HomePage;
 import com.ap.pages.SignInPage;
 
-public class Login extends TestBase{
+public class HomePageTests extends TestBase{
+	HomePage hp;
+	SignInPage sip;
 	@BeforeTest
 	public void initializer() throws IOException{
 		driver=initialization();
 
 	}
-	
 	@Test
+	public void validateRxRefillLinks() throws InterruptedException{
+		driver.get(propt.getProperty("url"));
+		hp = new HomePage(driver);
+		WebDriverWait wait = new WebDriverWait(driver, 15);//without explicit, its saying stale element
+		Actions a = new Actions(driver);
+		a.moveToElement(hp.clickRxRefillButton()).build().perform();
+		wait.until(ExpectedConditions.visibilityOf(hp.clickRxRefillButton()));
+		//Thread.sleep(5000);//To visually check if hovering is occuring
+		
+	}
+	
+	@Test(enabled=false)
 	public void login (){
 		driver.get(propt.getProperty("url"));
-		HomePage hp = new HomePage(driver);
+		hp = new HomePage(driver);
 
 		hp.clickYourAccount().click();
 		hp.clickSignIn().click();
-		SignInPage sip = new SignInPage(driver);
+		sip = new SignInPage(driver);
 		String SItitle=driver.getTitle();
 		Assert.assertEquals(SItitle, "Sign In or Register to Get Started Using Walgreens.com");
 
