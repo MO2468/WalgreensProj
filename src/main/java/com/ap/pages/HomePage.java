648b55111c4a461a20afa6017d59b7e966082d2f
mo2468 <islam.m1337@gmail.com>
2020-01-20 19:49:15 -0500

    roni user story in progress 1/20

--- src/main/java/com/ap/pages/HomePage.java
+++ src/main/java/com/ap/pages/HomePage.java
@@ -1,5 +1,7 @@
 package com.ap.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,8 +17,12 @@ public class HomePage {
 	WebElement youraccountbutton;
 	@FindBy(xpath="//*[contains(text(),'Sign in')]")
 	WebElement signinbutton;
+	@FindBy(xpath="//*[contains(text(),'Balance Rewards')]")
+	WebElement rewardsbalancebutton;
 	@FindBy(xpath="//*[contains(text(),'Rx Refills')]")
 	WebElement rxrefillbutton;
+	@FindBy(xpath="//*[contains(@data-kidx, 'm-1-')]")
+	List <WebElement> rxrefilllinks;
 	@FindBy(xpath="//input[@id='ntt-placeholder']")
 	WebElement searchBox;
 	@FindBy(xpath="//*[@type='submit']")
@@ -32,8 +38,14 @@ public class HomePage {
 	public WebElement clickSignIn(){
 		return signinbutton;
 	}
+	public WebElement clickRewardsBalance(){
+		return rewardsbalancebutton;
+	}
 	public WebElement clickRxRefillButton(){
 		return rxrefillbutton;
+	}
+	public List<WebElement> rxRefillLinks(){
+		return rxrefilllinks;
 	}
 	public WebElement clickSearchBoxtoType(){
 		return searchBox;
